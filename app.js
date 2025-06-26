e7c61ad312d7ebb3db3a06882e7f50e2c730b249
Zachary <zacharyblundell@live.com>
2025-06-26 14:51:56 +0200

    recreated

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { PORT } from "./config/env.js";
+
+import organizerRouter from "./routes/organizer.routes.js";
+import associationRouter from "./routes/association.routes.js";
+import authRouter from "./routes/auth.routes.js";
+
+const app = express();
+
+app.use(express.json());
+
+// Router prefixes
+app.use("/api/v1/organizers", organizerRouter);
+app.use("/api/v1/associations", associationRouter);
+app.use("/api/v1/auth", authRouter);
+
+app.get("/api/v1", (req, res) => {
+  res.send("Welcome to the Convergence des Lutte API!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}/api/v1`);
+});
+
+export default app;
