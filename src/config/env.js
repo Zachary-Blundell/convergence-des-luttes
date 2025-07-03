ab4f9664816e5de19a9462a85986a4dca483e153
Zachary <zacharyblundell@live.com>
2025-07-03 17:10:47 +0200

    moved

--- src/config/env.js
+++
@@ -1,6 +0,0 @@
-import { config } from "dotenv";
-
-config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
-
-export const { PORT, NODE_ENV, DB_URL, JWT_SECRET, JWT_EXPIRATION } =
-  process.env;
